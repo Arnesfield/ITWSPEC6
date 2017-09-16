b0f5e5922b62829443092fcf7820aa38b7103705
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-09-16 18:28:26 +0800

    Added database sql file

---
+++ sql/db_item.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 16, 2017 at 12:27 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_item`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_item`
+--
+
+CREATE TABLE `tbl_item` (
+  `item_id` int(12) NOT NULL,
+  `item_name` varchar(128) NOT NULL,
+  `item_desc` text NOT NULL,
+  `item_price` float(8,2) NOT NULL,
+  `item_added_at` int(11) NOT NULL,
+  `item_updated_at` int(11) NOT NULL,
+  `item_slug` varchar(128) NOT NULL,
+  `item_status` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_item`
+--
+
+INSERT INTO `tbl_item` (`item_id`, `item_name`, `item_desc`, `item_price`, `item_added_at`, `item_updated_at`, `item_slug`, `item_status`) VALUES
+(1, 'Apple', 'Red', 12.00, 1505557572, 1505557572, 'apple', '1'),
+(2, 'Banana', 'Yellow', 10.00, 1505557581, 1505557581, 'banana', '1'),
+(3, 'Orange', 'Sour', 13.00, 1505557595, 1505557595, 'orange', '1'),
+(4, 'Strawberry', 'Cute', 15.00, 1505557608, 1505557608, 'strawberry', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_item`
+--
+ALTER TABLE `tbl_item`
+  ADD PRIMARY KEY (`item_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_item`
+--
+ALTER TABLE `tbl_item`
+  MODIFY `item_id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
