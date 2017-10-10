6f46da36309d36655a71972199a0d391764d15a1
Jefferson Rylee <rylee.jeff385@gmail.com>
2017-10-10 13:44:32 +0800

    Added form validation

---
+++ codeigniter/application/controllers/Form.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Form extends MY_View_Controller {
+
+  public function __construct() {
+    parent::__construct();
+  }
+  
+  public function index() {
+    $this->load->library('form_validation');
+
+    $this->form_validation->set_rules('username', 'Username', 'trim|required');
+    $this->form_validation->set_rules('password', 'Password', 'trim|required');
+    $this->form_validation->set_rules('passconf', 'Password Confirmation', 'trim|required|matches[password]');
+    $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+
+    if ($this->form_validation->run() === FALSE) {
+      $this->_view('pages/form', 'Form');
+    }
+    else {
+      $this->_view('pages/form_success');
+    }
+
+  }
+}
+
+?>
